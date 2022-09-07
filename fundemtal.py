a7bc5ed1a99c45bf059968cecc8c825533bbda01
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-09-06 23:56:37 -0500

    data

--- fundemtal.py
+++ fundemtal.py
@@ -1,2 +1,29 @@
-#the world
-print('sup my shizzil mcJizzil')
+
+
+# tells you what the data is
+type()
+#emptyness or the absence think null
+None
+
+print(text='AlgoExpert is the best!')
+#utlisezes terminal to asing a value 
+#came be used in combatin with muples inputes
+# and interperaltion or concatination
+
+number = input('Number: ')
+age = input('Age: ')
+print('yo joe you number'+ number + 'makes sence you this' + age + 'years old')
+#will prefor a divsion and returns the integer result
+print(100 //6)
+
+ord() #takes string returns ASCII nuymber
+chr()# take ASCII  number return a letter
+
+or # is like the || in javascript
+not # this the ! in javascript
+and # uses condiion that works when both arguments are True
+
+list # its array in javascript
+
+in # allow check if element is present in diectionary, list, set
+    # if presetr returns True
