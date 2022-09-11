3aff594880803d5f3afe34a2455cc5992c104f82
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-09-11 13:19:12 -0500

    almost there

--- ex.py
+++ ex.py
@@ -8,4 +8,10 @@ first_slice = lst[::-1]
 # third_slice = second_slice[x:]
 last_slice = first_slice[::1]
 
-print(last_slice)
+
+
+strings=['touchMe', 'Sebsatain', 'heroHero', 'yuri alpha', 'Aura']
+
+arr=[]
+for idx in  range(1,len(strings)):
+    print(strings[idx])
